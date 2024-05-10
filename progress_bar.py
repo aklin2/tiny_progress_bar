c051a793814035035fb147a1b42661da2442a4ad
Alan Lin <lin.alan.k@gmail.com>
2024-05-09 18:27:12 -0700

    resolved some bugs in progress_bar.py

--- progress_bar.py
+++ progress_bar.py
@@ -18,23 +18,28 @@ def progress_bar(array: Iterable, bar_length: Characters = 50) -> Iterable:
 
     Raises:
         TypeError: array is not iterable.
+        ValueError: bar_length is too short.
 
     Yields:
         any: next(array)
     """
     if not isinstance(array, Iterable):
         raise TypeError(
-            "'array' is not iterable. Please supply an iterable such as an list, string, dictionary, etc."
+            "'array' is not iterable. Please supply an iterable such as a list, string, dictionary, etc."
+        )
+    if bar_length < 10:
+        raise ValueError(
+            f"Parameter 'bar_length' = {bar_length}. It needs to be at least 10 characters."
         )
 
     # length of the progress bar, does not include [, >, ], or the 6 characters for final percentage
     percentage = (1 / len(array)) * 100
-    progress = 0
+    progress = percentage
     array_iter = iter(array)
+
     while progress <= 100:
         # progress is marked by '=' and then closed with '>'
         # Fill gap with whitespace
-        progress += percentage
         num_of_bars = int(bar_length * (progress / 100))
         whitespace_length = int(bar_length - num_of_bars)
         bar = (
@@ -45,10 +50,27 @@ def progress_bar(array: Iterable, bar_length: Characters = 50) -> Iterable:
             + "]"
             + f" {progress:4.1f}%"
         )
+        # Reset
         sys.stdout.write(
-            "\b" * int(bar_length + 3 + 6)
-        )  # Add 3 for [, >, and ] and 6 for ' xx.x%'
-        sys.stdout.flush()
+            "\b" * int(bar_length + 3 + 7)
+        )  # Add 3 for [, >, and ] and 7 for ' xxx.x%'
+        # Write bar
         sys.stdout.write(bar)
         sys.stdout.flush()
         yield next(array_iter)
+        progress += percentage
+    # Clean up
+    sys.stdout.write("\n")
+    sys.stdout.flush()
+
+
+if __name__ == "__main__":
+    from time import sleep
+
+    array = list(range(0, 10))
+    count = 0
+    for i in progress_bar(array, 10):
+        sleep(0.01)
+        count += i
+    print(count)
+    print(sum(array))

31751cdc3d4e15ce65e912a87e5c5104ded1bb0c
Alan Lin <lin.alan.k@gmail.com>
2024-05-09 19:46:38 -0700

    modified setup

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from distutils.core import setup
 setup(
     name="tiny_progress_bar",  # How you named your package folder (MyLib)
     packages=["tiny_progress_bar"],  # Chose the same as "name"
-    version="0.1",  # Start with a small number and increase it with every change you make
+    version="0.1.1",  # Start with a small number and increase it with every change you make
     license="GPL-3.0",  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     description="Simple progress bar for Python 3",  # Give a short description about your library
     readme="README.md",
     author="Alan Lin",  # Type in your name
     author_email="lin.alan.k@gmail.com",  # Type in your E-Mail
     url="https://github.com/aklin2/tiny_progress_bar",  # Provide either the link to your github or to your website
-    download_url="https://github.com/aklin2/tiny_progress_bar/archive/refs/tags/v_01.tar.gz",  # I explain this later on
+    download_url="https://github.com/aklin2/tiny_progress_bar/archive/refs/tags/v0.1.1.tar.gz",  # I explain this later on
     keywords=[
         "progress",
         "progress bar",

8faa98931c54bf7c7b5e963149be561093eb7a72
Alan Lin <lin.alan.k@gmail.com>
2024-06-06 14:14:30 -0700

    modified setups, v0.1.6

--- setup.py
+++ setup.py
@@ -7,20 +7,24 @@ try:
 except (IOError, ImportError):
     long_description = open("README.md").read()
 
-version = "0.1.5"
+version = "0.1.6"
 
 setup(
     name="tiny_progress_bar",  # How you named your package folder (MyLib)
     packages=["tiny_progress_bar"],  # Chose the same as "name"
     version=version,  # Start with a small number and increase it with every change you make
-    license="GPL-3.0",  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-    description="Simple progress bar for Python 3",  # Give a short description about your library
+    # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+    license="GPL-3.0",
+    # Give a short description about your library
+    description="Simple progress bar for Python 3",
     long_description=long_description,
     readme="README.md",
     author="Alan Lin",  # Type in your name
     author_email="lin.alan.k@gmail.com",  # Type in your E-Mail
-    url="https://github.com/aklin2/tiny_progress_bar",  # Provide either the link to your github or to your website
-    download_url="https://github.com/aklin2/tiny_progress_bar/archive/refs/tags/v0.1.5.tar.gz",  # I explain this later on
+    # Provide either the link to your github or to your website
+    url="https://github.com/aklin2/tiny_progress_bar",
+    # I explain this later on
+    download_url="https://github.com/aklin2/tiny_progress_bar/archive/refs/tags/v0.1.5.tar.gz",
     keywords=[
         "progress",
         "progress bar",
@@ -30,10 +34,13 @@ setup(
         "loading_bar",
     ],  # Keywords that define your package best
     classifiers=[
-        "Development Status :: 3 - Alpha",  # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",  # Define that your audience are developers
         "Topic :: Software Development :: Build Tools",
-        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",  # Again, pick a license
-        "Programming Language :: Python :: 3",  # Specify which pyhton versions that you want to support
+        # Again, pick a license
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        # Specify which pyhton versions that you want to support
+        "Programming Language :: Python :: 3",
     ],
 )
